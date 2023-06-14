4b067e7d74d985017d6dc80348dd9bfe2f93f533
kamilkubiak2210 <kamil.kubiak4@outlook.com>
2023-06-14 18:38:50 +0200

    Rozwiązanie.

--- src/main/java/pl/javastart/euler2/Euler2.java
+++ src/main/java/pl/javastart/euler2/Euler2.java
@@ -3,6 +3,24 @@ package pl.javastart.euler2;
 public class Euler2 {
 
     public static void main(String[] args) {
+        int result = calculateTheSumOfTheFibonacciSequence(4_000_000);
+        System.out.println(result);
+    }
 
+    public static int calculateTheSumOfTheFibonacciSequence(int limit) {
+        int sum = 0;
+        int prev = 1;
+        int current = 2;
+
+        while (prev <= limit) {
+            if (prev % 2 == 0) {
+                sum += prev;
+            }
+
+            int next = prev + current;
+            prev = current;
+            current = next;
+        }
+        return sum;
     }
 }
